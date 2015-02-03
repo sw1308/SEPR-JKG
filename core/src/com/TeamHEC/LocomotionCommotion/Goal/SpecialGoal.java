d49b1fece5246440bcff8cd90fe9d054154e4041
em948 <em948@users.noreply.github.com>
2015-02-03 20:46:23 +0000

    setspecial
    setspecial should be set to true when a a special goal in initialized

--- core/src/com/TeamHEC/LocomotionCommotion/Goal/SpecialGoal.java
+++ core/src/com/TeamHEC/LocomotionCommotion/Goal/SpecialGoal.java
@@ -6,6 +6,7 @@ public class SpecialGoal extends Goal{
  
 	public SpecialGoal(Station startStation, Station finalStation, Station stationVia, String cargo, int reward2) {
 		this(startStation, finalStation, stationVia, cargo, reward2, 0);
+		this.setSpecial(true);
 	}
 	
 	 public SpecialGoal(Station startstation, Station finalStation, Station stationVia, String cargo, int reward2, int turnLimit)
