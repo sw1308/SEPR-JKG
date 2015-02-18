3c32defe31fbd22e338ef22088ba247e4c62140a
sw1308 <sw1308@york.ac.uk>
2015-02-18 03:33:21 +0000

    Fixed CardFactory to generate cards as normal again

--- core/src/com/TeamHEC/LocomotionCommotion/Card/CardFactory.java
+++ core/src/com/TeamHEC/LocomotionCommotion/Card/CardFactory.java
@@ -63,11 +63,10 @@ public class CardFactory{
 	 */
 	public Card createAnyCard()
 	{
-		//ArrayList<Card> cardList = new ArrayList<Card>(magicCardList);
-		//cardList.addAll(resourceCardList);
+		ArrayList<Card> cardList = new ArrayList<Card>(magicCardList);
+		cardList.addAll(resourceCardList);
 		
-		//return cardList.get(random.nextInt(cardList.size()));
-		return teleport;
+		return cardList.get(random.nextInt(cardList.size()));
 	}
 	
 	/**
